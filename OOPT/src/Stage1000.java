9afafcf2f749bec2b2374df8b2b30a53658cb0e7
jaehyun <jaehyun739@gmail.com>
2018-07-24 17:12:09 +0900

    stage1001 ui

--- OOPT/src/Stage1000.java
+++ OOPT/src/Stage1000.java
@@ -7,34 +7,14 @@ import javax.swing.border.EmptyBorder;
 
 public class Stage1000 extends JFrame {
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Stage1000 frame = new Stage1000();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the frame.
 	 */
 	public Stage1000() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
 	}
 
 }
